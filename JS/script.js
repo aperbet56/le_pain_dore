ffd9d9d038e4ce530c73fa2515d24d990710f8a4
Annabelle Perbet <annabelleperbet@yahoo.fr>
2026-09-11 19:49:09 +0200

    Mise à jour du code js

--- JS/script.js
+++ JS/script.js
@@ -3,27 +3,29 @@ const elementsToAnimate = document.querySelectorAll(".fade-in-element");
 
 /** ANIMATION FLUIDE AU DÉFILEMENT (SCROLL) */
 
-// On configure le détecteur visuel
-const observerOptions = {
-  root: null, // Utilise la fenêtre du navigateur comme zone de détection
-  rootMargin: "0px",
-  threshold: 0.15, // L'animation se déclenche quand 15% de la carte est visible
+// Création de l'objet options qui va définir les options de mon intersectionObserver
+const options = {
+  root: null,
+  rootMargin: "0px", // marges sur les 4 côtés de la zone d'affichage
+  threshold: 0.15,
 };
 
-// On crée le détecteur (Observer)
-const scrollObserver = new IntersectionObserver((entries, observer) => {
+// Déclaration de la fonction handleIntersect ayant comme paramètres entries et observer
+const handleIntersect = (entries, observer) => {
+  // Pour chaque entrée
   entries.forEach((entry) => {
-    // Si l'élément entre dans l'écran
     if (entry.isIntersecting) {
-      // On lui ajoute la classe qui le rend visible
       entry.target.classList.add("is-visible");
-      // On arrête de le surveiller pour économiser de la batterie/performance
+      // La méthode unobserve() de l'interface IntersectionObserver indique à l'objet IntersectionObserver courant de cesser d'observer l'élément cible spécifié.
       observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("is-visible");
     }
   });
-}, observerOptions);
+};
 
-// On lance la surveillance sur chaque carte de pain
-elementsToAnimate.forEach((element) => {
-  scrollObserver.observe(element);
-});
+// Création d'un nouvel observateur en appelant le constructeur IntersectionObserver() en précisant une fonction callback à appeler quand l'intersection franchit l'un de des paliers, handleIntersect(), et mon ensemble d'options.
+const observer = new IntersectionObserver(handleIntersect, options);
+
+// // Surveiller l'évolution de la visibilité de l'intersection de plusieurs éléments par rapport au viewport en appelant observer.observe() pour chacun de ces éléments.
+elementsToAnimate.forEach((element) => observer.observe(element));
